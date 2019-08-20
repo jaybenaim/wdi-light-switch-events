99763fc0c3b8ea3e9b8474b6bb24eaa893ecac21
jaybenaim <benaimjacob@gmail.com>
2019-08-20 14:10:32 -0400

    light switched on and off with color

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
-
-
+let counter = 0
+$('.switch').click(() => { 
+    if (counter % 2 == 0){ 
+        $('.switch').attr('class', 'switch off light')
+    } else if (counter % 1 == 0){ 
+        $('.switch').attr('class', 'switch on dark') 
+    }
+    counter++;
+})
