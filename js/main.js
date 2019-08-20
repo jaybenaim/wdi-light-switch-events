b94f5d2546db30438c5b78c03a7731f89f252af9
jaybenaim <benaimjacob@gmail.com>
2019-08-20 14:16:15 -0400

    changed body background color with switch

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 let counter = 0
 $('.switch').click(() => { 
     if (counter % 2 == 0){ 
-        $('.switch').attr('class', 'switch off light')
+        $('.switch').attr('class', 'switch off')
+        $('body').attr('class', 'dark')
     } else if (counter % 1 == 0){ 
-        $('.switch').attr('class', 'switch on dark') 
+        $('.switch').attr('class', 'switch on') 
+        $('body').attr('class', 'light')
     }
     counter++;
 })
