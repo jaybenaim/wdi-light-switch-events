e4c3e7a8ce63e81c4ac56098c1fbe0427070bd64
jaybenaim <benaimjacob@gmail.com>
2019-08-20 14:18:24 -0400

    changed text on switch click

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ $('.switch').click(() => {
     if (counter % 2 == 0){ 
         $('.switch').attr('class', 'switch off')
         $('body').attr('class', 'dark')
+        $('.status').text('Hey, who turned off the lights?')
     } else if (counter % 1 == 0){ 
         $('.switch').attr('class', 'switch on') 
         $('body').attr('class', 'light')
+        $('.status').text("It's so bright in here!")
     }
     counter++;
 })
